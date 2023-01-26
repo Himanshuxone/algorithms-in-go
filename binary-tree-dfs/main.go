fe5f3605977b673e0064917c166af4b643c46803
Himanshu <himanshusdec8@gmail.com>
2023-01-26 20:29:00 +0530

    dfs using directed binary tree/graph

--- binary-tree-dfs/main.go
+++ binary-tree-dfs/main.go
@@ -14,30 +14,29 @@ func main() {
 	// Output: 3
 	// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
 	// Output: 6
-	// Explanation: The LCA (Lowest Common ancestor) of nodes 2 and 8 is 6.
+	// Explanation: The LCA (Lowest Common ancestor) of nodes 2 and 8 isx 6.
 	var root *TreeNode
-	sl := []int{3, 9, 20, 0, 0, 15, 7}
-	for _, value := range sl {
-		root = createBinaryTree(root, value)
-	}
 	resp := make(map[int][]int, 0)
+	root = &TreeNode{Val: 6}
+	root.Left = AddNode(root.Left, 2)
+	root.Right = AddNode(root.Right, 8)
+	root.Left.Left = AddNode(root.Left.Left, 1)
+	root.Left.Right = AddNode(root.Left.Right, 4)
+	root.Left.Left.Right = AddNode(root.Left.Left.Right, 0)
+	root.Left.Left.Left = AddNode(root.Left.Left.Left, 0)
+	root.Left.Right.Left = AddNode(root.Left.Right.Left, 7)
+	root.Left.Right.Right = AddNode(root.Left.Right.Right, 9)
+	root.Right.Left = AddNode(root.Right.Left, 3)
+	root.Right.Right = AddNode(root.Right.Right, 5)
 	traverse(root, resp)
+	TreeTraversalDFS(root)
 }
 
-func createBinaryTree(node *TreeNode, val int) *TreeNode {
-	if node == nil {
-		if val != 0 {
-			// insert nill which means no child
-			node = &TreeNode{Val: val}
-		}
-		return node
+func AddNode(node *TreeNode, val int) *TreeNode {
+	if val == 0 {
+		node = nil
 	}
-
-	if val > node.Val {
-		node.Right = createBinaryTree(node.Right, val)
-	} else if val < node.Val {
-		node.Left = createBinaryTree(node.Left, val)
-	}
+	node = &TreeNode{Val: val}
 	return node
 }
 
@@ -58,28 +57,37 @@ func traverse(node *TreeNode, resp map[int][]int) {
 	traverse(node.Right, resp)
 }
 
-func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func TreeTraversalDFS(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
 	parent := make(map[*TreeNode]*TreeNode)
-	DFS(root.Left, p, q, parent)
-	DFS(root.Right, p, q, parent)
+	DFS(root.Left, parent)
+	DFS(root.Right, parent)
 	fmt.Println(parent)
 	return root
 }
 
-func DFS(node, p, q *TreeNode, visited map[*TreeNode]*TreeNode) {
+func DFS(node *TreeNode, visited map[*TreeNode]*TreeNode) {
 	if node == nil {
 		return
 	}
 
 	if node.Left != nil {
 		visited[node.Left] = node
-		DFS(node.Left, p, q, visited)
+		DFS(node.Left, visited)
 	}
 	if node.Right != nil {
 		visited[node.Right] = node
-		DFS(node.Right, p, q, visited)
+		DFS(node.Right, visited)
 	}
 }
